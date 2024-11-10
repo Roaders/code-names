8d95b73823f7ee38f5da5df85d1c4da2bf34ec1d
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2024-11-10 17:33:19 +0000

    randomise board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cell, Grid, Row } from './contracts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'codenames';
+    
+    public readonly grid: Grid<any>;
+
+    public readonly gridSize: number;
+    public readonly teamCount: number;
+    public readonly cardCount: number
+    public readonly assassinCount: number;
+
+    public assassins: Cell[];
+    public teamCells: Cell[][];
+
+    constructor(){
+
+        this.gridSize = 5;
+        this.teamCount = 2;
+        this.cardCount = 8
+        this.assassinCount = 1;
+
+        this.grid = generateGrid(this.gridSize);
+
+        const { assassins, teams} = this.generateGame();
+
+        this.assassins = assassins;
+        this.teamCells = teams
+
+        console.log(this.grid);
+    }
+
+    public getCellStyle(cell: Cell): string | undefined {
+        if(this.assassins.some(assassin => cellMatch(assassin, cell))){
+            return `bg-dark`
+        }
+        
+        if(this.teamCells[0].some(teamCard => cellMatch(teamCard, cell))){
+            return `bg-primary`
+        }
+        
+        if(this.teamCells[1].some(teamCard => cellMatch(teamCard, cell))){
+            return `bg-danger`
+        }
+
+
+
+        return `bg-warning bg-opacity-50`;
+    }
+
+    private generateGame(): {assassins: Cell[], teams: Cell[][]}{
+        const cells = this.grid.map(row => row.flat()).flat() as [Cell, ...Cell[]];
+
+        const assassins = Array.from({length: this.assassinCount}).map(() => randomItem(cells));
+
+        //TODO: for 2 player game generate extra cell for team that goes first
+        const teamOne = Array.from({length: this.cardCount}).map(() => randomItem(cells));
+        const teamTwo = Array.from({length: this.cardCount}).map(() => randomItem(cells));
+
+        const teams = [teamOne, teamTwo];
+
+        return { assassins, teams };
+    }
+
+}
+
+function cellMatch(one: Cell, two: Cell): boolean{
+    return one.column === two.column && one.row === two.row;
+}
+
+function randomItem<T>(items: [T, ...T[]]): T{
+    const randomIndex = Math.floor(Math.random() * items.length);
+
+    return items.splice(randomIndex, 1)[0];
+}
+
+function generateGrid<TSize extends number>(size: TSize): Grid<TSize>{
+    return Array.from({length: size}).map((_, row) => generateRow(size, row)) as Grid<TSize>;
+}
+
+function generateRow<TSize extends number>(size: TSize, row: number): Row<TSize>{
+    return Array.from({length: size}).map((_, column) => ({row, column})) as Row<TSize>;
 }
---
+++ src/app/contracts.ts
@@ -0,0 +1,10 @@
+
+type Tuple<T, N extends number> = number extends N ? T[] : TupleAppend<T, N, []>;
+type TupleAppend<T, N extends number, R extends unknown[]> = R['length'] extends N ? R : TupleAppend<T, N, [T, ...R]>;
+
+export type Cell = {row: string, column: string};
+
+export type Row<N extends number> = Tuple<Cell, N>;
+
+export type Grid<N extends number> = Tuple<Row<N>, N>;
+
