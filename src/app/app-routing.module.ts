43e1db10a4498ef6b68573d68cf2ab94081c9ba0
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2024-12-30 15:01:01 +0000

    convert to hash based routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { NewComponent } from './components/new-component/new-component.component
 import { GridComponent } from './components/grid/grid.component';
 
 const routes: Routes = [
-    { path: '', component: GridComponent }, // Default route
+    { path: '', component: GridComponent },
     { path: 'game/:teamCount/:gridSize/:cardCount/:assassinCount/:assassins/:teams', component: GridComponent },
     { path: 'new', component: NewComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
