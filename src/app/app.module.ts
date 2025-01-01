87140b0369e5df1f0705645fca0537a5d6bf806d
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2025-01-01 14:33:51 +0000

    Add version number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { GridComponent } from './components/grid/grid.component'; // Import GridComponent
+import { GridComponent } from './components/grid/grid.component';
+import packageJson from '../../package.json';
 
 @NgModule({
-    declarations: [AppComponent, GridComponent], // Declare GridComponent
+    declarations: [AppComponent, GridComponent],
     imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private titleService: Title) {
+        this.titleService.setTitle(`Codenames v${packageJson.version}`);
+    }
+}
